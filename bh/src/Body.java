0a0e33e9673861edb224e9d23914fdd64571d0dd
Mohsen Vakilian <reprogrammer@gmail.com>
2013-02-06 09:35:28 -0600

    Reformatted the source code of jolden/bh.
    Regenerated the class files and the jar file for jolden/bh.

--- bh/src/Body.java
+++ bh/src/Body.java
@@ -1,16 +1,14 @@
-
 import java.lang.Math;
 import java.util.Enumeration;
 
 /**
  * A class used to representing particles in the N-body simulation.
  **/
-public final class Body extends Node
-{
+public final class Body extends Node {
   MathVector vel;
   MathVector acc;
   MathVector newAcc;
-  double     phi;
+  double phi;
 
   private Body next;
   private Body procNext;
@@ -18,95 +16,92 @@ public final class Body extends Node
   /**
    * Create an empty body.
    **/
-  public Body()
-  {
-    vel      = new MathVector();
-    acc      = new MathVector();
-    newAcc   = new MathVector();
-    phi      = 0.0;
-    next     = null;
+  public Body() {
+    vel = new MathVector();
+    acc = new MathVector();
+    newAcc = new MathVector();
+    phi = 0.0;
+    next = null;
     procNext = null;
   }
 
   /**
    * Set the next body in the list.
+   * 
    * @param n the body
    **/
-  public final void setNext(Body n)
-  {
+  public final void setNext(Body n) {
     next = n;
   }
 
   /**
    * Get the next body in the list.
+   * 
    * @return the next body
    **/
-  public final Body getNext()
-  {
+  public final Body getNext() {
     return next;
   }
 
   /**
    * Set the next body in the list.
+   * 
    * @param n the body
    **/
-  public final void setProcNext(Body n)
-  {
+  public final void setProcNext(Body n) {
     procNext = n;
   }
 
   /**
    * Get the next body in the list.
+   * 
    * @return the next body
    **/
-  public final Body getProcNext()
-  {
+  public final Body getProcNext() {
     return procNext;
   }
 
   /**
    * Enlarge cubical "box", salvaging existing tree structure.
+   * 
    * @param tree the root of the tree.
    * @param nsteps the current time step
    **/
-  public final void expandBox(Tree tree, int nsteps)
-  {
+  public final void expandBox(Tree tree, int nsteps) {
     MathVector rmid = new MathVector();
 
     boolean inbox = icTest(tree);
     while (!inbox) {
       double rsize = tree.rsize;
       rmid.addScalar(tree.rmin, 0.5 * rsize);
-      
+
       for (int k = 0; k < MathVector.NDIM; k++) {
-	if (pos.value(k) < rmid.value(k)) {
-	  double rmin = tree.rmin.value(k);
-	  tree.rmin.value(k, rmin - rsize);
-	}
+        if (pos.value(k) < rmid.value(k)) {
+          double rmin = tree.rmin.value(k);
+          tree.rmin.value(k, rmin - rsize);
+        }
       }
       tree.rsize = 2.0 * rsize;
       if (tree.root != null) {
-	MathVector ic = tree.intcoord(rmid);
-	if (ic == null) throw new Error("Value is out of bounds");
-	int k = oldSubindex(ic, IMAX >> 1);
-	Cell newt = new Cell();
-	newt.subp[k] = tree.root;
-	tree.root = newt;
-	inbox = icTest(tree);
+        MathVector ic = tree.intcoord(rmid);
+        if (ic == null) throw new Error("Value is out of bounds");
+        int k = oldSubindex(ic, IMAX >> 1);
+        Cell newt = new Cell();
+        newt.subp[k] = tree.root;
+        tree.root = newt;
+        inbox = icTest(tree);
       }
     }
   }
 
   /**
-   * Check the bounds of the body and return true if it isn't in the
-   * correct bounds.
+   * Check the bounds of the body and return true if it isn't in the correct bounds.
    **/
-  public final boolean icTest(Tree tree)
-  {
+  public final boolean icTest(Tree tree) {
     double pos0 = pos.value(0);
     double pos1 = pos.value(1);
     double pos2 = pos.value(2);
-    
+
     // by default, it is in bounds
     boolean result = true;
 
@@ -129,16 +124,16 @@ public final class Body extends Node
   }
 
   /**
-   * Des end Tree and insert particle.  We're at a body so we need to
-   * create a cell and attach this body to the cell.
+   * Des end Tree and insert particle. We're at a body so we need to create a cell and attach this
+   * body to the cell.
+   * 
    * @param p the body to insert
    * @param xpic
-   * @param l 
+   * @param l
    * @param tree the root of the data structure
    * @return the subtree with the new body inserted
    **/
-  public final Node loadTree(Body p, MathVector xpic, int l, Tree tree)
-  {
+  public final Node loadTree(Body p, MathVector xpic, int l, Tree tree) {
     // create a Cell
     Cell retval = new Cell();
     int si = subindex(tree, l);
@@ -148,53 +143,66 @@ public final class Body extends Node
     // move down one level
     si = oldSubindex(xpic, l);
     Node rt = retval.subp[si];
-    if (rt != null) 
+    if (rt != null)
       retval.subp[si] = rt.loadTree(p, xpic, l >> 1, tree);
-    else 
+    else
       retval.subp[si] = p;
     return retval;
   }
 
   /**
    * Descend tree finding center of mass coordinates
+   * 
    * @return the mass of this node
    **/
-  public final double hackcofm()
-  {
+  public final double hackcofm() {
     return mass;
   }
 
   /**
    * Return an enumeration of the bodies
+   * 
    * @return an enumeration of the bodies
    **/
-  public final Enumeration elements()
-  {
+  public final Enumeration elements() {
     // a local class that implements the enumerator
     class Enumerate implements Enumeration {
       private Body current;
-      public Enumerate() { this.current = Body.this; }
-      public boolean hasMoreElements() { return (current != null);  }
+
+      public Enumerate() {
+        this.current = Body.this;
+      }
+
+      public boolean hasMoreElements() {
+        return (current != null);
+      }
+
       public Object nextElement() {
-	Object retval = current;
-	current = current.next;
-	return retval;
+        Object retval = current;
+        current = current.next;
+        return retval;
       }
     }
     return new Enumerate();
   }
 
-  public final Enumeration elementsRev()
-  {
+  public final Enumeration elementsRev() {
     // a local class that implements the enumerator
     class Enumerate implements Enumeration {
       private Body current;
-      public Enumerate() { this.current = Body.this; }
-      public boolean hasMoreElements() {  return (current != null);  }
+
+      public Enumerate() {
+        this.current = Body.this;
+      }
+
+      public boolean hasMoreElements() {
+        return (current != null);
+      }
+
       public Object nextElement() {
-	Object retval = current;
-	current = current.procNext;
-	return retval;
+        Object retval = current;
+        current = current.procNext;
+        return retval;
       }
     }
 
@@ -202,12 +210,11 @@ public final class Body extends Node
   }
 
   /**
-   * Determine which subcell to select.
-   * Combination of intcoord and oldSubindex.
+   * Determine which subcell to select. Combination of intcoord and oldSubindex.
+   * 
    * @param t the root of the tree
    **/
-  public final int subindex(Tree tree, int l)
-  {
+  public final int subindex(Tree tree, int l) {
     MathVector xp = new MathVector();
 
     double xsc = (pos.value(0) - tree.rmin.value(0)) / tree.rsize;
@@ -221,21 +228,19 @@ public final class Body extends Node
 
     int i = 0;
     for (int k = 0; k < MathVector.NDIM; k++) {
-      if (((int)xp.value(k) & l) != 0) {
-	i += Cell.NSUB >> (k + 1);
+      if (((int) xp.value(k) & l) != 0) {
+        i += Cell.NSUB >> (k + 1);
       }
     }
     return i;
   }
 
   /**
-   * Evaluate gravitational field on the body.
-   * The original olden version calls a routine named "walkscan",
-   * but we use the same name that is in the Barnes code.
+   * Evaluate gravitational field on the body. The original olden version calls a routine named
+   * "walkscan", but we use the same name that is in the Barnes code.
    **/
-  public final void hackGravity(double rsize, Node root)
-  {
-    MathVector pos0 = (MathVector)pos.clone();
+  public final void hackGravity(double rsize, Node root) {
+    MathVector pos0 = (MathVector) pos.clone();
 
     HG hg = new HG(this, pos);
     hg = root.walkSubTree(rsize * rsize, hg);
@@ -246,21 +251,18 @@ public final class Body extends Node
   /**
    * Recursively walk the tree to do hackwalk calculation
    **/
-  public final HG walkSubTree(double dsq, HG hg)
-  {
-    if (this != hg.pskip)
-      hg = gravSub(hg);
+  public final HG walkSubTree(double dsq, HG hg) {
+    if (this != hg.pskip) hg = gravSub(hg);
     return hg;
   }
 
   /**
    * Return a string represenation of a body.
+   * 
    * @return a string represenation of a body.
    **/
-  public String toString()
-  {
+  public String toString() {
     return "Body " + super.toString();
   }
 
 }
-
--- bh/src/Cell.java
+++ bh/src/Cell.java
@@ -1,62 +1,57 @@
-
 /**
  * A class used to represent internal nodes in the tree
  **/
-public final class Cell extends Node
-{
+public final class Cell extends Node {
   // subcells per cell
-  public final static int NSUB = 8;  // 1 << NDIM
+  public final static int NSUB = 8; // 1 << NDIM
 
   /**
-   * The children of this cell node.  Each entry may contain either 
-   * another cell or a body.
+   * The children of this cell node. Each entry may contain either another cell or a body.
    **/
   Node[] subp;
-  Cell   next;
+  Cell next;
 
-  public Cell()
-  {
+  public Cell() {
     subp = new Node[NSUB];
     next = null;
   }
-  
+
   /**
-   * Descend Tree and insert particle.  We're at a cell so 
-   * we need to move down the tree.
+   * Descend Tree and insert particle. We're at a cell so we need to move down the tree.
+   * 
    * @param p the body to insert into the tree
    * @param xpic
    * @param l
    * @param tree the root of the tree
    * @return the subtree with the new body inserted
    **/
-  public final Node loadTree(Body p, MathVector xpic, int l, Tree tree)
-  {
+  public final Node loadTree(Body p, MathVector xpic, int l, Tree tree) {
     // move down one level
     int si = oldSubindex(xpic, l);
     Node rt = subp[si];
-    if (rt != null) 
+    if (rt != null)
       subp[si] = rt.loadTree(p, xpic, l >> 1, tree);
-    else 
+    else
       subp[si] = p;
     return this;
   }
 
   /**
    * Descend tree finding center of mass coordinates
+   * 
    * @return the mass of this node
    **/
-  public final double hackcofm()
-  {
+  public final double hackcofm() {
     double mq = 0.0;
     MathVector tmpPos = new MathVector();
-    MathVector tmpv   = new MathVector();
-    for (int i=0; i < NSUB; i++) {
+    MathVector tmpv = new MathVector();
+    for (int i = 0; i < NSUB; i++) {
       Node r = subp[i];
       if (r != null) {
-	double mr = r.hackcofm();
-	mq = mr + mq;
-	tmpv.multScalar(r.pos, mr);
-	tmpPos.addition(tmpv);
+        double mr = r.hackcofm();
+        mq = mr + mq;
+        tmpv.multScalar(r.pos, mr);
+        tmpPos.addition(tmpv);
       }
     }
     mass = mq;
@@ -69,13 +64,11 @@ public final class Cell extends Node
   /**
    * Recursively walk the tree to do hackwalk calculation
    **/
-  public final HG walkSubTree(double dsq, HG hg)
-  {
+  public final HG walkSubTree(double dsq, HG hg) {
     if (subdivp(dsq, hg)) {
       for (int k = 0; k < Cell.NSUB; k++) {
-	Node r = subp[k];
-	if (r != null)
-	  hg = r.walkSubTree(dsq / 4.0, hg);
+        Node r = subp[k];
+        if (r != null) hg = r.walkSubTree(dsq / 4.0, hg);
       }
     } else {
       hg = gravSub(hg);
@@ -85,10 +78,10 @@ public final class Cell extends Node
 
   /**
    * Decide if the cell is too close to accept as a single term.
+   * 
    * @return true if the cell is too close.
    **/
-  public final boolean subdivp(double dsq, HG hg)
-  {
+  public final boolean subdivp(double dsq, HG hg) {
     MathVector dr = new MathVector();
     dr.subtraction(pos, hg.pos0);
     double drsq = dr.dotProduct();
@@ -99,10 +92,10 @@ public final class Cell extends Node
 
   /**
    * Return a string represenation of a cell.
+   * 
    * @return a string represenation of a cell.
    **/
-  public String toString()
-  {
+  public String toString() {
     return "Cell " + super.toString();
   }
 
