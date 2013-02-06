9b0db3f159493715153935044af4809574fbeb9f
Mohsen Vakilian <reprogrammer@gmail.com>
2013-02-06 14:17:39 -0600

    Reformatted and compiled em3d.

--- em3d/src/BiGraph.java
+++ em3d/src/BiGraph.java
@@ -1,13 +1,10 @@
-
 import java.util.Enumeration;
 
-/** 
- * A class that represents the irregular bipartite graph used in
- * EM3D.  The graph contains two linked structures that represent the
- * E nodes and the N nodes in the application.
+/**
+ * A class that represents the irregular bipartite graph used in EM3D. The graph contains two linked
+ * structures that represent the E nodes and the N nodes in the application.
  **/
-public final class BiGraph
-{
+public final class BiGraph {
   /**
    * Nodes that represent the electrical field.
    **/
@@ -19,25 +16,24 @@ public final class BiGraph
 
   /**
    * Construct the bipartite graph.
+   * 
    * @param e the nodes representing the electric fields
    * @param h the nodes representing the magnetic fields
-   **/ 
-  public BiGraph(Node e, Node h)
-  {
+   **/
+  public BiGraph(Node e, Node h) {
     eNodes = e;
     hNodes = h;
   }
 
   /**
-   * Create the bi graph that contains the linked list of
-   * e and h nodes.
+   * Create the bi graph that contains the linked list of e and h nodes.
+   * 
    * @param numNodes the number of nodes to create
    * @param numDegree the out-degree of each node
    * @param verbose should we print out runtime messages
    * @return the bi graph that we've created.
    **/
-  public static BiGraph create(int numNodes, int numDegree, boolean verbose)
-  {
+  public static BiGraph create(int numNodes, int numDegree, boolean verbose) {
     Node.initSeed(783);
 
     // making nodes (we create a table)
@@ -47,32 +43,32 @@ public final class BiGraph
 
     // making neighbors
     if (verbose) System.out.println("updating from and coeffs");
-    for (Enumeration e = hTable[0].elements(); e.hasMoreElements(); ) {
+    for (Enumeration e = hTable[0].elements(); e.hasMoreElements();) {
       Node n = (Node) e.nextElement();
       n.makeUniqueNeighbors(eTable);
     }
-    for (Enumeration e = eTable[0].elements(); e.hasMoreElements(); ) {
+    for (Enumeration e = eTable[0].elements(); e.hasMoreElements();) {
       Node n = (Node) e.nextElement();
       n.makeUniqueNeighbors(hTable);
     }
 
     // Create the fromNodes and coeff field
     if (verbose) System.out.println("filling from fields");
-    for (Enumeration e = hTable[0].elements(); e.hasMoreElements(); ) {
+    for (Enumeration e = hTable[0].elements(); e.hasMoreElements();) {
       Node n = (Node) e.nextElement();
       n.makeFromNodes();
     }
-    for (Enumeration e = eTable[0].elements(); e.hasMoreElements(); ) {
+    for (Enumeration e = eTable[0].elements(); e.hasMoreElements();) {
       Node n = (Node) e.nextElement();
       n.makeFromNodes();
     }
 
     // Update the fromNodes
-    for (Enumeration e = hTable[0].elements(); e.hasMoreElements(); ) {
+    for (Enumeration e = hTable[0].elements(); e.hasMoreElements();) {
       Node n = (Node) e.nextElement();
       n.updateFromNodes();
     }
-    for (Enumeration e = eTable[0].elements(); e.hasMoreElements(); ) {
+    for (Enumeration e = eTable[0].elements(); e.hasMoreElements();) {
       Node n = (Node) e.nextElement();
       n.updateFromNodes();
     }
@@ -81,17 +77,15 @@ public final class BiGraph
     return g;
   }
 
-  /** 
-   * Update the field values of e-nodes based on the values of
-   * neighboring h-nodes and vice-versa.
+  /**
+   * Update the field values of e-nodes based on the values of neighboring h-nodes and vice-versa.
    **/
-  public void compute()
-  {
-    for (Enumeration e = eNodes.elements(); e.hasMoreElements(); ) {
+  public void compute() {
+    for (Enumeration e = eNodes.elements(); e.hasMoreElements();) {
       Node n = (Node) e.nextElement();
       n.computeNewValue();
     }
-    for (Enumeration e = hNodes.elements(); e.hasMoreElements(); ) {
+    for (Enumeration e = hNodes.elements(); e.hasMoreElements();) {
       Node n = (Node) e.nextElement();
       n.computeNewValue();
     }
@@ -99,17 +93,17 @@ public final class BiGraph
 
   /**
    * Override the toString method to print out the values of the e and h nodes.
+   * 
    * @return a string contain the values of the e and h nodes.
    **/
-  public String toString()
-  {
+  public String toString() {
     StringBuffer retval = new StringBuffer();
-    for (Enumeration e = eNodes.elements(); e.hasMoreElements(); ) {
+    for (Enumeration e = eNodes.elements(); e.hasMoreElements();) {
       Node n = (Node) e.nextElement();
       retval.append("E: " + n + "\n");
     }
-    
-    for (Enumeration e = hNodes.elements(); e.hasMoreElements(); ) {
+
+    for (Enumeration e = hNodes.elements(); e.hasMoreElements();) {
       Node n = (Node) e.nextElement();
       retval.append("H: " + n + "\n");
     }
--- em3d/src/Graph.java
+++ em3d/src/Graph.java
@@ -1,11 +1,8 @@
-
-/** 
- * A class that represents the irregular bipartite graph used in
- * EM3D.  The graph contains two linked structures that represent the
- * E nodes and the N nodes in the application.
+/**
+ * A class that represents the irregular bipartite graph used in EM3D. The graph contains two linked
+ * structures that represent the E nodes and the N nodes in the application.
  **/
-public class Graph
-{
+public class Graph {
   /**
    * Nodes that represent the electrical field.
    **/
